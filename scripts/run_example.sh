e2ec2941f33902cdf88288b08f8555e930fbcb71
Oliver <oliver.klapper@elektrobit.com>
2024-03-08 17:04:47 +0100

    Fix output manifest

--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -4,4 +4,4 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 base_dir="$script_dir/.."
 
 mkdir -p "$base_dir/out/"
-ie --verbose --manifest tests/manifests/example.yml --output out/out.yml
+ie --verbose --manifest tests/manifests/example.yml --output out/out
