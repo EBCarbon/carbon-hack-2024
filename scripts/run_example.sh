dd921ccca74376a579a0a7eda067aa4554000257
Oliver <oliver.klapper@elektrobit.com>
2024-03-08 16:45:52 +0100

    Integrate breaking changes

--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -4,4 +4,4 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 base_dir="$script_dir/.."
 
 mkdir -p "$base_dir/out/"
-impact-engine --impl "tests/manifests/example.yml" --ompl "out/example.yml"
+ie --verbose --manifest tests/manifests/example.yml --output out/out.yml
